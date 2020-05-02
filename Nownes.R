e619403d6392289670a3cf085065ae4686e20fb9
John Nownes <jnownes@iastate.edu>
2020-05-02 18:20:28 -0500

    Working on joining fips and population data to raw data

--- Nownes.R
+++ Nownes.R
@@ -1,13 +1,61 @@
 library(lubridate)
+library(dplyr)
+library(ggplot2)
 
 dat = read.csv("us_states_covid19_daily.csv", stringsAsFactors = FALSE)
-dist = (read.csv("social_distancing.csv", header=F)[-c(1:3,56:66),])
-headers = read.csv("social_distancing.csv", header = F,stringsAsFactors = F)[c(3),]
-colnames(dist) =c(headers[1,])
-
 dat$date = ymd(dat$date)
 dat$state = as.factor(dat$state)
+dat$fips = as.factor(dat$fips)
+dat = dat %>%
+  filter(fips != '60' & fips != '66' & fips != '69' & fips != '72' & fips != '78')
+dat$fips = factor(dat$fips, levels= dat$fips, labels=dat$fips)
+str(dat$fips)
+levels(dat$fips)
 
-dist$Location = factor(dist$Location,levels= dist$Location)
-str(dat)
+dist = (read.csv("social_distancing.csv", header=F)[-c(1:4,56:66),])
+dist_headers = read.csv("social_distancing.csv", header = F,stringsAsFactors = F)[c(3),]
+colnames(dist) =c(dist_headers[1,])
+names(dist)[1] = "state"
+dist$state = factor(dist$state,levels= dist$state,labels = dist$state)
 str(dist)
+
+pop = read.csv("state_population.csv", header = F)[-c(1:9,61:67),]
+pop_headers = read.csv("state_population.csv", header = F,stringsAsFactors = F)[c(4),]
+colnames(pop) = c(pop_headers[1,])
+names(pop)[1] = "state"
+pop$state = sub('.', '', pop$state)
+pop$state = as.factor(pop$state)
+pop$`2019` = as.numeric(gsub(",", "", pop$`2019`))
+pop$`2019` = as.numeric(pop$`2019`)
+str(pop)
+
+Fips = read.csv("fips.csv", header=F)[-c(1:5),]
+Fips_headers = read.csv("fips.csv",header=F,stringsAsFactors = F)[c(5),]
+colnames(Fips) = c(Fips_headers[1,])
+Fips = Fips %>%
+  filter(`Summary Level` == '040') %>%
+  select(`State Code (FIPS)`, state = `Area Name (including legal/statistical area description)`) %>%
+  filter(state != 'Puerto Rico')
+names(Fips)[1] = "fips"
+#START HERE
+Fips$fips = factor(Fips$fips,levels= Fips$fips,labels = Fips$fips)
+Fips$state = factor(Fips$state, levels = Fips$state, labels = Fips$state)
+
+
+dist = pop %>%
+  select("state","2019") %>%
+  inner_join(dist)
+names(dist)[2] = "population"
+dist$state = as.factor(dist$state)
+
+dist = Fips %>%
+  inner_join(dist)
+
+
+
+levels(dist$fips)
+levels(dat$fips)
+
+
+dat %>%
+  ggplot(aes(x=date,y= positive)) + geom_point() + facet_wrap(~state)
